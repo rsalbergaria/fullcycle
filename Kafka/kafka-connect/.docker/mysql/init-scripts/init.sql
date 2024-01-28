0b4e9e55b8d7e1dbacfb49a3cd3e14eecdda61b0
Gabrielle Santos Albergaria <gabriellesantosalbergaria@Gabrielles-MacBook-Air.local>
2024-01-28 18:45:54 -0300

    mysql connect

---
+++ Kafka/kafka-connect/.docker/mysql/init-scripts/init.sql
@@ -0,0 +1,21 @@
+USE fullcycle;
+
+-- Criação do usuário 'nodeuser' com todas as permissões
+CREATE USER 'gabrielle'@'%' IDENTIFIED BY 'gabrielle';
+
+-- Concede todas as permissões ao usuário 'nodeuser' de qualquer host
+GRANT ALL PRIVILEGES ON *.* TO 'gabrielle'@'%' WITH GRANT OPTION;
+
+-- Atualiza as permissões
+FLUSH PRIVILEGES;
+
+-- Utiliza o banco de dados nodedb
+USE fullcycle;
+
+CREATE TABLE IF NOT EXISTS CATEGORIES (
+    id INT auto_increment,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO CATEGORIES (name) VALUES ('Eletronicos');
